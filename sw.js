bcb321f86d06eeda06676a07a7d0975d0869a7a2
vaki-spec <vlkiefer@hotmail.de>
2026-08-18 09:38:12 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C="kt-v1";const A=["./","./index.html","./manifest.json"];self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
